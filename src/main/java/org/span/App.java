c245778c23f85026584d153919bb808869a5945c
TestSmirk <d.kc@qq.com>
2019-02-14 12:01:12 +0800

    off banner

--- src/main/java/org/span/App.java
+++ src/main/java/org/span/App.java
@@ -1,11 +1,15 @@
 package org.span;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplicationBuilder springApplicationBuilder = new SpringApplicationBuilder(App.class);
+        springApplicationBuilder.bannerMode(Banner.Mode.OFF).run(args);
+
     }
 }
